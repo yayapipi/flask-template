3f2bd0589ebd7fe1404f713c423af1bc504101ea
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-11-15 20:49:34 +0800

    fix host

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return "Welcome, this is a Flask app deployed on Zeabur"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
