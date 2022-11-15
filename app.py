27562bd7ac93f402903f72972239e75687b6000e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-11-15 16:33:36 +0800

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome, this is a Flask app deployed on Zeabur"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
